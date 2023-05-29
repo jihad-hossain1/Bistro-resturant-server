b32265d71e55de0818780ebc3870a165dcea78e4
jihad-hossain1 <jihadkham934@gmail.com>
2023-05-29 23:50:53 +0600

    delete from ui and database from my cart list

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
@@ -56,6 +56,12 @@ async function run() {
             const item = req.body;
             console.log(item);
             const result = await cartCollection.insertOne(item);
+            res.send(result)
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
         // Send a ping to confirm a successful connection
