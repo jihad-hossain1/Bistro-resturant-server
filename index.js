632330c492f4ada2a83a029eb89827946b61c07a
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-30 11:34:42 +0600

    which user already create a account not accept data from client to server side with database

--- index.js
+++ index.js
@@ -30,14 +30,20 @@ async function run() {
         const menuCollection = client.db('bistroRestaurant').collection('menu')
         const reviewCollection = client.db('bistroRestaurant').collection('reviews')
         const cartCollection = client.db('bistroRestaurant').collection('carts')
-        
-        
+
+
         app.post('/users', async (req, res) => {
             const user = req.body
+            console.log(user);
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
-        
+
         // menu api
         app.get('/menus', async (req, res) => {
             const result = await menuCollection.find().toArray()
