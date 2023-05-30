aaf79f795a3b9f5ebf49af3407f9addcb42e4067
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-30 17:06:39 +0600

    verify jwt token authorized or unauthorized user ?

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+// middleware for verify jwt token
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCES_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xd4auwc.mongodb.net/?retryWrites=true&w=majority`;
@@ -84,11 +101,15 @@ async function run() {
         });
 
         // cart collections api
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
 
             if (!email) {
                 res.send([]);
+            }
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'providen access' })
             }
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
